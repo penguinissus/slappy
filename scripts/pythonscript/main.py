ae75400c84af4b5cef11ee5f868ecbe247f677c7
Sophia Duan <duansophia8@gmail.com>
2025-11-15 16:43:51 -0500

    um maybe

---
+++ scripts/pythonscript/main.py
@@ -0,0 +1,36 @@
+# Source - https://stackoverflow.com/a
+# Posted by Umer Farooq, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-11-15, License - CC BY-SA 4.0
+
+import websocket
+import time
+import threading
+def on_message(ws, message):
+    print(message) # sensor data here in JSON format
+
+def on_error(ws, error):
+    print("### error ###")
+    print(error)
+
+def on_close(ws, close_code, reason):
+    print("### closed ###")
+    print("close code : ", close_code)
+    print("reason : ", reason  )
+
+def on_open(ws):
+    print("connection opened")
+    
+def stopafter(ws,sec):
+    time.sleep(sec)
+    ws.close()
+    print('closed boom')
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp("ws://Sophia.civichall.org:8080/sensor/connect?type=android.sensor.accelerometer",
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close)
+    
+    threading.Thread(target=stopafter, args=(ws, 4)).start()
+    ws.run_forever()
