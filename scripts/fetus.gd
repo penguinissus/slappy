12d3a5e9b080e295cfdff25c1bd79157ea087d78
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2025-11-18 10:02:44 -0500

    recreated fetus, resized camera, kept things proper size, collision works

--- scripts/fetus.gd
+++ scripts/fetus.gd
@@ -1,39 +1,24 @@
 extends CharacterBody2D
-@onready var _crawl_animation = $AnimatedSprite2D
-@onready var sprite = $AnimatedSprite2D
-const SPEED = 700.0
-const JUMP_VELOCITY = -900.0
-
-func _ready() -> void:
-	add_to_group("player")
+#annotations check grandma
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+		
 	if Input.is_action_just_pressed("fetus_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
 	var direction := Input.get_axis("fetus_left", "fetus_right")
 	if direction:
 		velocity.x = direction * SPEED
-		sprite.flip_h = direction > 0		
-		_crawl_animation.play()
-
-		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		_crawl_animation.stop()
+		
 	move_and_slide()
-
-
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	print("yes")
-	get_tree().change_scene_to_file("res://slapaboom.tscn")
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().name == "grandma":
+			get_tree().change_scene_to_file("res://slapaboom.tscn")
