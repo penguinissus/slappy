38ba71ebeec500ea99bd8d5b9e4580ce73f144f4
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2025-11-18 17:24:25 -0500

    map update again

--- scripts/fetus.gd
+++ scripts/fetus.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 #annotations check grandma
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -700.0
 
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * 2 * delta
 		
 	if Input.is_action_just_pressed("fetus_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
--- scripts/grandma.gd
+++ scripts/grandma.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0 #how high th player goes
+const JUMP_VELOCITY = -700.0 #how high th player goes
 
 func _physics_process(delta: float) -> void: #delta = time since the last frame
 	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * 2 * delta
 		
 	if Input.is_action_just_pressed("grandma_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
